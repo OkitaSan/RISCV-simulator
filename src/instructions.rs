2bd4d41e97dadbdeb4aab6c8a04c932ba84984de
OkitaSan <tssjy999@163.com>
2021-07-25 17:02:33 +0800

    itype added

--- src/instructions.rs
+++ src/instructions.rs
@@ -1,4 +1,4 @@
-use crate::util::get_digits;
+use crate::util::{get_digits, set_one_digits, set_zero_digits};
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 use std::ops::{Deref, DerefMut, Shl, Shr, Sub};
@@ -35,6 +35,7 @@ pub enum Instructions {
 ///
 ///  Basic operation of the instruction, and this abbreviation is its traditional name
 ///
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct RType(pub u32);
 /// R-type debug only,due to the limitation of serde-rs
 #[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
@@ -123,6 +124,129 @@ impl RType {
             Self::OPCODE_LEN + Self::RD_LEN + Self::FUNCT3_LEN + Self::RS1_LEN,
             Self::OPCODE_LEN + Self::RD_LEN + Self::FUNCT3_LEN + Self::RS1_LEN + Self::RS2_LEN - 1,
         )
+    }
+    #[inline]
+    pub fn get_rs1(&self) -> u32 {
+        get_digits(
+            self.0,
+            Self::OPCODE_LEN + Self::RD_LEN + Self::FUNCT3_LEN,
+            Self::OPCODE_LEN + Self::RD_LEN + Self::FUNCT3_LEN + Self::RS1_LEN - 1,
+        )
+    }
+    #[inline]
+    pub fn get_funct3(&self) -> u32 {
+        get_digits(
+            self.0,
+            Self::OPCODE_LEN + Self::RD_LEN,
+            Self::OPCODE_LEN + Self::RD_LEN + Self::FUNCT3_LEN - 1,
+        )
+    }
+    #[inline]
+    pub fn get_rd(&self) -> u32 {
+        get_digits(
+            self.0,
+            Self::OPCODE_LEN,
+            Self::OPCODE_LEN + Self::RD_LEN - 1,
+        )
+    }
+    #[inline]
+    pub fn get_opcode(&self) -> u32 {
+        get_digits(self.0, 0, Self::OPCODE_LEN - 1)
+    }
+}
+
+/// The I-type instruction in RISC-V
+/// instruction format: |immediate|rs1|funct3|rd|opcode|
+/// ## Instruction format
+/// `immediate`: 12
+///  
+///  It will be interpreted as an 2's complement value
+/// `rs1`: 5
+///
+///  An operand register , and could be used as the base register when performing the ld operation
+///
+/// `funct3`: 3
+///
+///  An additional opcode field.
+///
+/// `rd` : 5
+///
+///  The destination register , and the value read from `ld` will be stored by rd
+///
+/// `opcode` : 7  
+///
+///  Basic operation of the instruction, and this abbreviation is its traditional name
+///
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct IType(pub u32);
+/// I-type debug only,due to the limitation of serde-rs
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug)]
+pub struct ITypeDebug {
+    pub immediate: i16,
+    pub rs1: u8,
+    pub funct3: u8,
+    pub rd: u8,
+    pub opcode: u8,
+}
+impl From<IType> for ITypeDebug{
+    fn from(i_inst: IType) -> Self {
+        ITypeDebug{
+            immediate:i_inst.get_immediate() as i16,
+            rs1:i_inst.get_rs1() as u8,
+            funct3:i_inst.get_funct3() as u8,
+            rd:i_inst.get_rd() as u8,
+            opcode:i_inst.get_opcode() as u8
+        }
+    }
+}
+impl From<u32> for IType {
+    fn from(inst: u32) -> Self {
+        IType(inst)
+    }
+}
+impl Deref for IType {
+    type Target = u32;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl DerefMut for IType {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+impl IType {
+    pub const IMMEDIATE_LEN: u32 = 12;
+    pub const RS1_LEN: u32 = 5;
+    pub const FUNCT3_LEN: u32 = 3;
+    pub const RD_LEN: u32 = 5;
+    pub const OPCODE_LEN: u32 = 7;
+    pub fn new(immediate: i16, rs1: u8, funct3: u8, rd: u8, opcode: u8) -> IType {
+        let immediate = u16::from_le_bytes(immediate.to_le_bytes());
+        let mut instruction = 0u32;
+        instruction |= (opcode & 0b0111_1111) as u32;
+        instruction |= ((rd & 0b0001_1111) as u32) << Self::OPCODE_LEN;
+        instruction |= ((funct3 & 0b0000_0111) as u32) << (Self::OPCODE_LEN + Self::RD_LEN);
+        instruction |=
+            ((rs1 & 0b0001_1111) as u32) << (Self::OPCODE_LEN + Self::RD_LEN + Self::FUNCT3_LEN);
+        instruction |= ((immediate & 0b0000_1111_1111_1111) as u32)
+            << (Self::OPCODE_LEN + Self::RD_LEN + Self::FUNCT3_LEN + Self::RS1_LEN);
+        IType(instruction)
+    }
+    #[inline]
+    pub fn get_immediate(&self) -> i32 {
+        let mut res = get_digits(
+            self.0,
+            Self::OPCODE_LEN + Self::RD_LEN + Self::FUNCT3_LEN + Self::RS1_LEN,
+            31,
+        );
+        if get_digits(res, 11, 11) == 1 {
+            res = set_one_digits(res, 12, 31);
+        } else {
+            res = set_zero_digits(res, 12, 31);
+        }
+        i32::from_le_bytes(res.to_le_bytes())
     }
     #[inline]
     pub fn get_rs1(&self) -> u32 {
--- src/lib.rs
+++ src/lib.rs
@@ -7,71 +7,115 @@ pub mod instructions;
 pub mod util;
 
 /// memory size
-pub const MEMORY_SIZE:usize = 134217728;
+pub const MEMORY_SIZE: usize = 134217728;
 /// memory(byte array)
-pub static MEMORY:[u8;MEMORY_SIZE] = [0;MEMORY_SIZE];
+pub static MEMORY: [u8; MEMORY_SIZE] = [0; MEMORY_SIZE];
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
     #[test]
-    fn test_get_digit_works(){
+    fn test_get_digit_works() {
         let case1 = 0b110011010000000000000000u32;
-        assert_eq!(util::get_digits(case1, 16, 19),0b1101);
-        assert_eq!(util::get_digits(case1, 0, 31),0b110011010000000000000000u32);
+        assert_eq!(util::get_digits(case1, 16, 19), 0b1101);
+        assert_eq!(
+            util::get_digits(case1, 0, 31),
+            0b110011010000000000000000u32
+        );
         // test 'gentleman's agreement'
-        assert_eq!(util::get_digits(case1, 0, 0),0);
-        assert_eq!(util::get_digits(case1, 16, 16),1);
-        assert_eq!(util::get_digits(case1, 31, 31),0);
+        assert_eq!(util::get_digits(case1, 0, 0), 0);
+        assert_eq!(util::get_digits(case1, 16, 16), 1);
+        assert_eq!(util::get_digits(case1, 31, 31), 0);
     }
     #[test]
-    fn test_rtype(){
-        let r_inst1:instructions::RType = 0b0000000_10111_00111_000_01100_0110011u32.into();
-        let r_inst2:instructions::RType = 0b0100000_01001_10110_000_10110_0110011u32.into();
-        let r_inst3 = instructions::RTypeDebug::from(instructions::RType::new(0b0000000, 0b10111, 0b00111, 0b000, 0b01100, 0b0110011));
-        let r_inst4 = instructions::RTypeDebug::from(instructions::RType::new(0b0100000,0b01001,0b10110,0b000,0b10110,0b0110011));
+    fn test_rtype() {
+        let r_inst1: instructions::RType = 0b0000000_10111_00111_000_01100_0110011u32.into();
+        let r_inst2: instructions::RType = 0b0100000_01001_10110_000_10110_0110011u32.into();
+        let r_inst3 = instructions::RTypeDebug::from(instructions::RType::new(
+            0b0000000, 0b10111, 0b00111, 0b000, 0b01100, 0b0110011,
+        ));
+        let r_inst4 = instructions::RTypeDebug::from(instructions::RType::new(
+            0b0100000, 0b01001, 0b10110, 0b000, 0b10110, 0b0110011,
+        ));
         assert_eq!(
             instructions::RTypeDebug::from(r_inst1),
-            instructions::RTypeDebug{
-                funct7:0b0000000,
-                rs2:0b10111,
-                rs1:0b00111,
-                funct3:0b000,
-                rd:0b01100,
-                opcode:0b0110011
+            instructions::RTypeDebug {
+                funct7: 0b0000000,
+                rs2: 0b10111,
+                rs1: 0b00111,
+                funct3: 0b000,
+                rd: 0b01100,
+                opcode: 0b0110011
             }
         );
         assert_eq!(
             instructions::RTypeDebug::from(r_inst2),
-            instructions::RTypeDebug{
-                funct7:0b0100000,
-                rs2:0b01001,
-                rs1:0b10110,
-                funct3:0b000,
-                rd:0b10110,
-                opcode:0b0110011
+            instructions::RTypeDebug {
+                funct7: 0b0100000,
+                rs2: 0b01001,
+                rs1: 0b10110,
+                funct3: 0b000,
+                rd: 0b10110,
+                opcode: 0b0110011
             }
         );
         assert_eq!(
             instructions::RTypeDebug::from(r_inst3),
-            instructions::RTypeDebug{
-                funct7:0b0000000,
-                rs2:0b10111,
-                rs1:0b00111,
-                funct3:0b000,
-                rd:0b01100,
-                opcode:0b0110011
+            instructions::RTypeDebug {
+                funct7: 0b0000000,
+                rs2: 0b10111,
+                rs1: 0b00111,
+                funct3: 0b000,
+                rd: 0b01100,
+                opcode: 0b0110011
             }
         );
         assert_eq!(
             instructions::RTypeDebug::from(r_inst4),
-            instructions::RTypeDebug{
-                funct7:0b0100000,
-                rs2:0b01001,
-                rs1:0b10110,
-                funct3:0b000,
-                rd:0b10110,
-                opcode:0b0110011
+            instructions::RTypeDebug {
+                funct7: 0b0100000,
+                rs2: 0b01001,
+                rs1: 0b10110,
+                funct3: 0b000,
+                rd: 0b10110,
+                opcode: 0b0110011
+            }
+        );
+    }
+    #[test]
+    fn test_itype() {
+        use instructions::{IType, ITypeDebug};
+        let inst1 = ITypeDebug::from(IType::new(-20, 0b11011, 0b000, 0b00110, 0b0010011));
+        let inst2 = ITypeDebug::from(IType::new(240, 10, 3, 9, 3));
+        let inst3 = ITypeDebug::from(IType::new(1, 9, 0, 9, 19));
+        assert_eq!(
+            inst1,
+            ITypeDebug {
+                immediate: -20,
+                rs1: 0b11011,
+                funct3: 0b000,
+                rd: 0b00110,
+                opcode: 0b0010011
+            }
+        );
+        assert_eq!(
+            inst2,
+            ITypeDebug {
+                immediate: 240,
+                rs1: 10,
+                funct3: 3,
+                rd: 9,
+                opcode: 3
+            }
+        );
+        assert_eq!(
+            inst3,
+            ITypeDebug {
+                immediate: 1,
+                rs1: 9,
+                funct3: 0,
+                rd: 9,
+                opcode: 19
             }
         );
     }
